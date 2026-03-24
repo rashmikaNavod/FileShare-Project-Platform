4b82b3aa00a22a063d50170ad0045f2d46515b45
rashmikaNavod <drashmikanavod@gmail.com>
2026-03-24 10:58:18 +0530

    Add ecosystem configuration for config-server, service-registry, and api-gateway

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    apps: [
+        {
+            name: "config-server",
+            script: "java",
+            args: "-jar ./config-server/target/config-server-1.0.0.jar",
+            log_file: "./logs/config-server.log",
+        },
+        {
+            name: "service-registry",
+            script: "java",
+            args: "-jar ./service-registry/target/service-registry-1.0.0.jar",
+            log_file: "./logs/service-registry.log",
+        },
+        {
+            name: "api-gateway",
+            script: "java",
+            args: "-jar ./api-gateway/target/api-gateway-1.0.0.jar",
+            log_file: "./logs/api-gateway.log",
+        }
+    ]
+}
